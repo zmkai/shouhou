45f83af171adf6052a47594065b2b54e91f652db
zmkai <1937915896@qq.com>
2018-11-02 09:19:22 +0800

    提交代码

--- shouhou/settings.py
+++ shouhou/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -81,13 +81,21 @@ WSGI_APPLICATION = 'shouhou.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'shouhou',
+    #     'USER':'postgres',
+    #     'PASSWORD':'123456',
+    #     'HOST':'192.168.6.82',
+    #     'PORT':'5432',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shouhou',
-        'USER':'postgres',
-        'PASSWORD':'123456',
-        'HOST':'192.168.6.82',
-        'PORT':'5432',
+        'USER': 'postgres',
+        'PASSWORD': '157382',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -153,3 +161,4 @@ LOGGING = {
         },
     },
 }
+
